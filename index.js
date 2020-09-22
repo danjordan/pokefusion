6fbdeca2ba024adfba2af251b23f6da6c5f727ba
Dan Jordan <daniel.jordan@mindera.com>
2020-09-22 11:59:36 +0100

    Remove agent

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 #!/usr/bin/env node
 
 const ora = require("ora");
-const https = require("https");
 const fetch = require("node-fetch");
 const termImg = require("term-img");
 const cheerio = require("cheerio");
 
-const agent = new https.Agent({
-  rejectUnauthorized: false,
-});
-
 (async () => {
-  const args = process.argv.slice(2);
+  try {
+    const args = process.argv.slice(2);
 
-  const url = ["https://pokemon.alexonsager.net", ...args].join("/");
+    const url = ["https://pokemon.alexonsager.net", ...args].join("/");
 
-  const spinner = ora("Fusing Pokémon...").start();
+    const spinner = ora("Fusing Pokémon...").start();
 
-  const html = await fetch(url, { agent }).then((res) => res.text());
-  const $ = cheerio.load(html);
+    const html = await fetch(url).then((res) => res.text());
+    const $ = cheerio.load(html);
 
-  const pokemon = {
-    name: $("#pk_name").text(),
-    src: $("#pk_img").attr("src"),
-  };
+    const pokemon = {
+      name: $("#pk_name").text(),
+      src: $("#pk_img").attr("src"),
+    };
 
-  const image = await fetch(pokemon.src, { agent });
-  const buffer = await image.buffer();
+    const image = await fetch(pokemon.src);
+    const buffer = await image.buffer();
 
-  spinner.stop();
+    spinner.stop();
 
-  console.log(await termImg(buffer));
-  console.log(pokemon.name);
+    console.log(await termImg(buffer));
+    console.log(pokemon.name);
+  } catch (e) {
+    console.log(`An error occurred. ${e.message}`);
+  }
 })();
